60794ee4696248d05662694d56c1416a959e6cc1
Pololo04 <pol.perez.prades@estudiantat.upc.edu>
2024-06-04 16:53:35 +0200

    spacy done

--- spacy_embed.py
+++ spacy_embed.py
@@ -1,2 +1,12 @@
-import spacy
+from typing import List, Tuple, Callable
+import numpy as np
 
+
+def map_spacy_embed(sentence_pairs: List[Tuple[str, str, float]], model_spacy: Callable
+                ) -> List[Tuple[Tuple[np.ndarray, np.ndarray], float]]:
+    pairs_vectors = []
+    for (s1, s2, score) in sentence_pairs:
+        s1_embed = model_spacy(s1).vector
+        s2_embed = model_spacy(s2).vector
+        pairs_vectors.append(((s1_embed, s2_embed), score))
+    return pairs_vectors
